89b36e9bc922d1aa2eafc16929bb4d5384abb380
karthick <karthickfaf19.7@gmail.com>
2025-07-17 12:32:19 +0100

    Update insertionsort.java

--- insertionsort.java
+++ insertionsort.java
@@ -1,22 +1,20 @@
-public class Main{
-    public static void main(String[] args){
-        int arr[] = {3,4,5,2,1};
-        
-        
-        for(int i = 1 ; i < arr.length ; i++){
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {3, 4, 5, 2, 1};
+
+        for (int i = 1; i < arr.length; i++) {
             int key = arr[i];
             int j = i - 1;
-            
-            while(j > 0 && arr[j] > key){
+
+            while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
-                
             }
-            
-            arr[j + 1]  = key;
+
+            arr[j + 1] = key;
         }
-        
-        for(int a : arr){
+
+        for (int a : arr) {
             System.out.print(a + " ");
         }
     }
